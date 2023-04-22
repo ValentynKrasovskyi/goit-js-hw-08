34e180ab8a93b4c5600ab048cbf8dcd55eae2178
ValentynKrasovskyi <krasovskyi00@gmail.com>
2023-04-22 19:57:58 +0200

    part 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
 
+const videoEl = document.querySelector('#vimeo-player');
+
+const player = new Player(videoEl);
+console.log(player)
+
+
+player.on('play', function () {
+        console.log('played the video!');
+    });
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+
+player.on('timeupdate', function () {
+    localStorage.setItem('videoplayer-current-time', timeupdate)
+    console.log(localStorage.getItem(videoplayerCurrentTime))
+    })
